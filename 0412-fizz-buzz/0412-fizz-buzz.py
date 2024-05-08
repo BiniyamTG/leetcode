a1ec6e66f9ab0459ce65dcf286a192652cf66c91
Biniyam <160533459+BiniyamTG@users.noreply.github.com>
2024-05-08 11:01:35 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        new=[]
+        if (n%3==0 and n%5!=0):
+            return "Fizz"
+        elif(n%5==0 and n%3!=0):
+            return "Buzz"
+        elif(n%5==0 and n%3==0):
+            return "FizzBuzz"
+        else:
+            return n
+        
